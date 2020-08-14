76401e4f6996f2d1f755a30fe416cce31c520204
Cody Gramlich <cmgramli@ualberta.ca>
2020-08-13 21:34:18 -0600

    Add args to build_packet function of pod telemetry program

--- pod_telemetry_program/main.cpp
+++ pod_telemetry_program/main.cpp
@@ -1,24 +1,39 @@
 #include <Ethernet2.h>
-#include <EthernetUdp2.h>
 
 #define PACKET_LENGTH 34
-#define DELAY 10
-
 #define ACCELERATION_IDX 2
 #define POSITION_IDX 6
 #define VELOCITY_IDX 10
 #define BATTERY_VOLTAGE_IDX 14
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress local_ip(192, 168, 2, 177);
-IPAddress remote_ip(192, 168, 2, 30);
+IPAddress local_ip(192, 168, 1, 177);
+IPAddress remote_ip(192, 168, 1, 84);
 
 unsigned int local_port = 3000;
 unsigned int remote_port = 3000;
 
 EthernetUDP Udp;
 
-uint8_t TEAM_ID = 0;
+unsigned long wait_time = 40; // 40 ms
+
+uint8_t team_id = 0;
+
+enum Status {
+              Fault = 0,
+              SafeToApproach = 1,
+              ReadyToLaunch = 2,
+              Launching = 3,
+              Coasting = 4,
+              Braking = 5,
+              Crawling = 6
+            };
+
+uint8_t status = Status::SafeToApproach;
+
+double position = 0;
+double velocity = 0;
+double acceleration = 0;
 
 // TODO: implement this function
 uint8_t get_status() {
@@ -40,24 +55,21 @@ uint32_t get_acceleration() {
   return 435468763;
 }
 
-uint8_t *build_packet() {
+uint8_t *build_packet(uint8_t team_id, uint8_t status, int32_t acceleration, int32_t position, int32_t velocity) {
   static uint8_t packet[PACKET_LENGTH];
-  packet[0] = TEAM_ID;
-  packet[1] = get_status();
+  packet[0] = team_id;
+  packet[1] = status;
 
-  uint32_t acceleration = get_acceleration();
   for (int i = ACCELERATION_IDX + 3; i >= ACCELERATION_IDX; i--) {
     packet[i] = acceleration & 0xFF;
     acceleration = acceleration >> 8;
   }
 
-  uint32_t position = get_position();
   for (int i = POSITION_IDX + 3; i >= POSITION_IDX; i--) {
     packet[i] = position & 0xFF;
     position = position >> 8;
   }
 
-  uint32_t velocity = get_velocity();
   for (int i = VELOCITY_IDX + 3; i >= VELOCITY_IDX; i--) {
     packet[i] = velocity & 0xFF;
     velocity = velocity >> 8;
@@ -77,8 +89,14 @@ void setup() {
 }
 
 void loop() {
+  status = get_status();
+  position = get_position();
+  velocity = get_velocity();
+  acceleration = get_acceleration();
+
   Udp.beginPacket(remote_ip, remote_port);
-  Udp.write(build_packet(), (size_t) PACKET_LENGTH);
+  uint8_t *packet = build_packet(team_id, status, (int32_t) acceleration, (int32_t) position, (int32_t) velocity);
+  Udp.write(packet, (size_t) PACKET_LENGTH);
   Udp.endPacket();
-  delay(DELAY);
+  delay(wait_time);
 }
